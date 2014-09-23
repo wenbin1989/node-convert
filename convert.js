439cccad86e2782b63361d30113914097cc22515
wenbin1989 <wenbin1989@gmail.com>
2014-09-23 18:47:45 +0800

    add error handling.

--- convert.js
+++ convert.js
@@ -3,44 +3,56 @@ var im = require('imagemagick');
 var url = require('url');
 var querystring = require('querystring');
 
-http.createServer(function (req, res) {
+http.createServer(function (request, response) {
     var postData = '';
-    var objectUrl = url.parse(req.url);
+    var objectUrl = url.parse(request.url);
     var objectQuery = querystring.parse(objectUrl.query);
     var type = objectQuery['type'];
 
-    if (typeof type === 'undefined') {
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.end('Missing required parameters: type');
+    if (request.method !== 'POST') {
+        response.writeHead(405, {'Content-Type': 'text/plain'});
+        response.end('Only allowed POST method.');
         return;
     }
 
-    req.setEncoding('binary');
-    req.addListener('data', function (postDataChunk) {
+    if (typeof type === 'undefined') {
+        response.writeHead(400, {'Content-Type': 'text/plain'});
+        response.end('Missing required parameters: type.');
+        return;
+    }
+
+    request.setEncoding('binary');
+    request.addListener('data', function (postDataChunk) {
         postData += postDataChunk;
     });
     // 所有数据包接收完毕
-    req.addListener('end', function () {
+    request.addListener('end', function () {
         if (postData.length === 0) {
-            res.writeHead(400, {'Content-Type': 'text/plain'});
-            res.end('Missing post src data.');
+            response.writeHead(400, {'Content-Type': 'text/plain'});
+            response.end('Missing post src data.');
             return;
         }
-        im.resize({
-            srcData: postData,
-            format: type,
-            density: 240,
-            quality: 1.0,
-            antialias: false
-        }, function(err, stdout) {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end(err);
-            } else {
-                res.writeHead(200, {'Content-Type': 'application/octet-stream'});
-                res.end(stdout, 'binary');
-            }
-        });
+
+        try {
+            im.resize({
+                srcData: postData,
+                format: type,
+                density: 240,
+                quality: 1.0,
+                antialias: false
+            }, function(err, stdout) {
+                if (err) {
+                    response.writeHead(500, {'Content-Type': 'text/plain'});
+                    response.end('Image convert error.');
+                } else {
+                    response.writeHead(200, {'Content-Type': 'application/octet-stream'});
+                    response.end(stdout, 'binary');
+                }
+            });
+        } catch (err) {
+            response.writeHead(500, {'Content-Type': 'text/plain'});
+            response.end('server error.');
+        }
     });
 }).listen(1337);
 console.log('Server running at port 1337');
